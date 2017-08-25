a6bbef82b7df7e873bcf5c10eba30e62d6ba06c9
bsrirangaswamy <30527688+bsrirangaswamy@users.noreply.github.com>
2017-08-25 07:35:12 -0500

    Initial Commit

---
+++ PocketPrompterTests/PocketPrompterTests.swift
@@ -0,0 +1,36 @@
+//
+//  PocketPrompterTests.swift
+//  PocketPrompterTests
+//
+//  Created by Balakumaran Srirangaswamy on 8/25/17.
+//  Copyright © 2017 Bala. All rights reserved.
+//
+
+import XCTest
+@testable import PocketPrompter
+
+class PocketPrompterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
