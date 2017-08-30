e4e8a30a544e91c1a56aa0136aac5f31c5bd6790
bsrirangaswamy <30527688+bsrirangaswamy@users.noreply.github.com>
2017-08-30 08:35:06 -0500

    Adding Frameworks
    => Embeddded the entire storyboard in a navigation controller.
    => Added Tessarct OCR framework and created a bridging frame work to access the Objective - C methods.

---
+++ PocketPrompter/PhantomBridger.m
@@ -0,0 +1,9 @@
+//
+//  PhantomBridger.m
+//  PocketPrompter
+//
+//  Created by Balakumaran Srirangaswamy on 8/25/17.
+//  Copyright © 2017 Bala. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
